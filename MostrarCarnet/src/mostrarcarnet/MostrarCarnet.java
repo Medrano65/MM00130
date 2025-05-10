230dfd196af6f7d66b757251bc8f01d9a8d0e8ab
Francisco Serrano <america.medrano65@gmail.com>
2025-05-09 19:43:18 -0600

    My first commit

---
+++ MostrarCarnet/src/mostrarcarnet/MostrarCarnet.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mostrarcarnet;
+
+/**
+ *
+ * @author frank
+ */
+public class MostrarCarnet {
+
+   // MostrarCarnet.java
+// Proyecto simple que imprime el nombre y carné estudiantil
+
+
+    public static void main(String[] args) {
+        String carnet = "MM00130";
+
+        System.out.println("El número de Carnet es: " + carnet);
+        
+    }
+}
+
